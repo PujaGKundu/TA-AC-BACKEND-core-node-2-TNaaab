3a079ed7f80fc23935800010c4e63a6206346304
PujaGKundu <pujakundu100@gmail.com>
2022-01-24 18:03:14 +0530

    Assignmet III: Core Node Part II

--- block-BNaabg/Project/server.js
+++ block-BNaabg/Project/server.js
@@ -1,24 +1,68 @@
 const http = require("http");
+var fs = require("fs");
+var url = require("url");
 
-const express = require("express");
+const server = http.createServer(handleRequest);
 
-//var cors = require("cors");
+var userPath = __dirname + "/users/";
 
-const usersRouter = require("./users/user");
+function handleRequest(req, res) {
+  var parsedUrl = url.parse(req.url, true);
 
-const app = express();
+  var store = "";
+  req.on("data", (chunk) => {
+    store += chunk;
+  });
 
-app.use(express.json());
+  req.on("end", () => {
+    if (req.url === "/users" && req.method === "POST") {
+      var username = JSON.parse(store).username;
+      fs.open(userPath + username + ".json", "wx", (err, fd) => {
+        if (err) return console.log(err);
+        fs.writeFile(fd, store, (err) => {
+          if (err) return console.log(err);
+          fs.close(fd, () => {
+            res.end(`${username} created successfully`);
+          });
+        });
+      });
+    }
 
-//app.use(cors({ origin: "http://localhost:8100" }));
+    if (parsedUrl.pathname === "/users" && req.method === "GET") {
+      var username = parsedUrl.query.username;
+      fs.readFile(userPath + username + ".json", (err, content) => {
+        if (err) return console.log(err);
+        res.setHeader("Content-Type", "application/json");
+        res.end(content);
+      });
+    }
 
-app.use("/user", usersRouter);
+    if (parsedUrl.pathname === "/users" && req.method === "PUT") {
+      var username = parsedUrl.query.username;
+      fs.open(userPath + username + ".json", "r+", (err, fd) => {
+        if (err) return console.log(err);
+        fs.ftruncate(fd, (err) => {
+          if (err) return console.log(err);
+          fs.writeFile(fd, store, (err) => {
+            if (err) return console.log(err);
+            fs.close(fd, () => {
+              res.end(`${username} updated successfully`);
+            });
+          });
+        });
+      });
+    }
 
-app.use("/", function (req, res) {
-  res.send("Node is working");
-});
+    if (parsedUrl.pathname === "/users" && req.method === "DELETE") {
+      var username = parsedUrl.query.username;
+      fs.unlink(userPath + username + ".json", (err) => {
+        if (err) return console.log(err);
+        res.end(`${username} deleted successfully`);
+      });
+    }
+  });
+}
 
-const server = http.createServer(app);
 server.listen(3000, () => {
   console.log("Server listening to port 3000");
 });
